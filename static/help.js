1a130d6317ac6a251f46e65ee9f864cc64667c63
John Glynn <playingallday383@gmail.com>
2023-06-28 12:14:55 -0400

    Major Update

---
+++ static/help.js
@@ -0,0 +1,54 @@
+    var currentURL = window.location.origin;
+
+    var codeElement = document.getElementById('url-one');
+    codeElement.textContent = currentURL + '/?app=color';
+
+    var currentURL = window.location.origin;
+      
+    var codeElement = document.getElementById('url-two');
+    codeElement.textContent = currentURL + '/?app=settings';
+
+    var currentURL = window.location.origin;
+      
+    var codeElement = document.getElementById('url-three');
+    codeElement.textContent = currentURL + '/?app=help';
+
+    var currentURL = window.location.origin;
+      
+    var codeElement = document.getElementById('url-four');
+    codeElement.textContent = currentURL + '/?app=canvas';
+
+    var currentURL = window.location.origin;
+      
+    var codeElement = document.getElementById('url-five');
+    codeElement.textContent = currentURL + '/?app=yt';
+
+    var currentURL = window.location.origin;
+      
+    var codeElement = document.getElementById('url-six');
+    codeElement.textContent = currentURL + '/?app=code';
+
+    var currentURL = window.location.origin;
+      
+    var codeElement = document.getElementById('url-seven');
+    codeElement.textContent = currentURL + '/?app=cmd';
+
+    var currentURL = window.location.origin;
+      
+    var codeElement = document.getElementById('url-eight');
+    codeElement.textContent = currentURL + '/?app=terminal';
+
+    var currentURL = window.location.origin;
+      
+    var codeElement = document.getElementById('url-nine');
+    codeElement.textContent = currentURL + '/?app=video';
+
+    var currentURL = window.location.origin;
+      
+    var codeElement = document.getElementById('url-ten');
+    codeElement.textContent = currentURL + '/?app=text';
+
+    var currentURL = window.location.origin;
+      
+    var codeElement = document.getElementById('url-elleven');
+    codeElement.textContent = currentURL + '/?app=changelog';
